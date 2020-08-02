15974238bc6782d0741713f70c035efc427d28b9
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-08-02 22:10:36 +0300

    Add project files.

---
+++ ShopApp.Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.Entities
+{
+   public class Product
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
