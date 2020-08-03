64b13ac2d86229c1f87daafee82825d915cc51cf
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-08-03 13:57:03 +0300

    Added generic repository

---
+++ ShopApp.DataAccess/Concrete/EfCore/EfCoreGenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+    public class EfCoreGenericRepository<T> : IGenericDal<T> where T : class, IEntity, new()
+    {
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetOne(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+   public class ShopContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server =DESKTOP-3VB3SSC\MSSQLSERVERLAST;Database = ShopDb;Integrated Security = true ");
+        }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
